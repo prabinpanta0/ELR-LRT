c76116b5b0059ec7849b5649b045ed9f116e576e
prabinpanta0 <pantaprabin30@gmail.com>
2025-03-02 14:58:46 +0545

    Enhance Python package setup and bindings; add C++17 support and new classes for patching and frequency analysis

--- cpp/src/bindings.cpp
+++ cpp/src/bindings.cpp
@@ -1,10 +1,22 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "patcher.h"
+#include "../include/patcher.h"
+#include "../include/frequency_table.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(dbpm, m) {
-    m.def("patch_sequence", &patch_sequence, "Patch the byte sequence based on entropy",
-          py::arg("bytes"), py::arg("k"), py::arg("theta"), py::arg("theta_r"));
+    m.doc() = "ELR-LRT sequence patching module";
+    
+    py::class_<Patcher>(m, "Patcher")
+        .def(py::init<>())
+        .def("patch_sequence", &Patcher::patch_sequence)
+        .def("get_patched_sequence", &Patcher::get_patched_sequence)
+        .def("reset", &Patcher::reset);
+
+    py::class_<FrequencyTable>(m, "FrequencyTable")
+        .def(py::init<>())
+        .def("add_sequence", &FrequencyTable::add_sequence)
+        .def("get_frequency", &FrequencyTable::get_frequency)
+        .def("clear", &FrequencyTable::clear);
 }
